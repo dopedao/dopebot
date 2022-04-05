c4828866a9c7035c6749795cba7a1b54607eecad
Oscar Guertler <oscarguertler@gmail.com>
2022-04-05 12:32:43 +0200

    export qx eth conv value

--- commands/quixotic/qx.js
+++ commands/quixotic/qx.js
@@ -1,5 +1,5 @@
 const { sfetch } = require("../../util/sfetch");
-const { quixoticDwApi } = require("../../constants");
+const { quixoticDwApi, qxEthConvValue } = require("../../constants");
 const { quixoticApiKey } = require("../../config.json");
 
 module.exports = {
@@ -7,12 +7,11 @@ module.exports = {
     description: "Quixotic Test commands",
     args: "(1-8000)",
     async execute(message) {
-        const convValue = 1000000000;
         const collectionData = await sfetch(quixoticDwApi, { headers: { "X-API-KEY": quixoticApiKey } });
         if (!collectionData) {
             return Promise.reject();
         }
 
-        await message.channel.send(`Floor: ${collectionData.floor_price / convValue}\nTrading Volume: ${(collectionData.volume_traded / convValue).toFixed(2)}`);
+        await message.channel.send(`Floor: ${collectionData.floor_price / qxEthConvValue}\nTrading Volume: ${(collectionData.volume_traded / qxEthConvValue).toFixed(2)}`);
     }
 };
