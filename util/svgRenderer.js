378d328f6a8866576004d2dd05eb82aa1d6113d4
Oscar Guertler <oscarguertler@gmail.com>
2022-04-09 23:30:51 +0200

    add images for explain embeds

--- util/svgRenderer.js
+++ util/svgRenderer.js
@@ -3,7 +3,7 @@ const Sharp = require('sharp');
 exports.svgRenderer = async (svg) => {
     const strippedSvg = svg.replace("data:image/svg+xml;base64,", '');
     const encodedSvg = Buffer.from(strippedSvg, "base64");
-    const hustlerPng = await Sharp(Buffer.from(encodedSvg)).png();
+    const hustlerPng = Sharp(Buffer.from(encodedSvg)).png();
 
     return hustlerPng;
 }
