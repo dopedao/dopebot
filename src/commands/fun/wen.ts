0bbc4091ef51655850492a8f32afce62c2223873
Oscar Guertler <oscarguertler@gmail.com>
2022-06-18 22:24:22 +0200

    update placeholder link

--- src/commands/fun/wen.ts
+++ src/commands/fun/wen.ts
@@ -26,7 +26,7 @@ const sendGameEmbed = async (interaction: CommandInteraction<CacheType>): Promis
     const wenGameEmbed = new MessageEmbed()
         .setTitle("Wen game ❓❓")
         .setColor("NOT_QUITE_BLACK")
-        .setDescription("The game is actively in the design and development stage.\nSince it is a community led project with many moving parts,\nthere is no solid date for the game release.\nThe best way to track our progress is via our wiki page ([Roadmap](https://google.com)), DopeDao [updates](https://dope-wars.notion.site/0121b6435eb84e869231501295d729bd?v=f493d6b27ffd4816861308561197702a) and discord channels.")
+        .setDescription("The game is actively in the design and development stage.\nSince it is a community led project with many moving parts,\nthere is no solid date for the game release.\nThe best way to track our progress is via our wiki page ([Roadmap](https://dopewars.gg/about)), DopeDao [updates](https://dope-wars.notion.site/0121b6435eb84e869231501295d729bd?v=f493d6b27ffd4816861308561197702a) and discord channels.")
         .setThumbnail(Constants.DW_THUMBNAIL);
 
     await interaction.reply({ embeds: [wenGameEmbed], ephmeral: false } as InteractionReplyOptions);
