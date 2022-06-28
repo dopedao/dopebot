1d1eca83dbeaac16faa84538327904f3b792fdf9
Oscar Guertler <oscarguertler@gmail.com>
2022-06-28 23:34:50 +0200

    adjust to new api

--- src/redis/sub.ts
+++ src/redis/sub.ts
@@ -4,70 +4,89 @@ import { logger } from "../util/logger";
 
 const log = logger("redis");
 
-interface IDiscordUser {
-    username: string,
-    discriminator: string,
-    id: string,
-    // Email?
-    email: string,
-    paperCount: number,
-    dopeCount: number,
-    hustlerCount: number,
-    isOg: boolean
+interface IDopeBotData {
+    Id: string,
+    WalletAddress: string,
+    PaperCount: number,
+    DopeCount: number,
+    HustlerCount: number,
+    IsOg: boolean,
 }
 
-const getPaperRole = (paperCount: number): (string | null) => {
-    switch(true) {
+const ogRole = 2623462346;
+const dopeHolderRole = 124353253;
+const hustlerHolderRole = 124353253;
+
+enum DopeRoles {
+    "Drug_Lord" = 234215,
+    "Kingpin",
+    "Fixer",
+    "Lieutenant",
+    "Hitman",
+    "Robber",
+    "Supplier",
+    "Dealer",
+    "Hopper",
+    "Prospect"
+}
+
+
+const getPaperRole = (paperCount: number): (DopeRoles| null) => {
+    switch (true) {
         case paperCount >= 5000000:
-            return "Drug Lord";
+            return DopeRoles.Drug_Lord;
         case paperCount < 5000000 && paperCount >= 2000000:
-            return "Kingpin";
+            return DopeRoles.Kingpin;
         case paperCount < 2000000 && paperCount >= 1000000:
-            return "Fixer";
+            return DopeRoles.Fixer;
         case paperCount < 1000000 && paperCount >= 750000:
-            return "Lieutenant";
+            return DopeRoles.Lieutenant;
         case paperCount < 750000 && paperCount >= 500000:
-            return "Hitman";
+            return DopeRoles.Hitman;
         case paperCount < 500000 && paperCount >= 350000:
-            return "Enforcer";
+            return DopeRoles.Robber;
         case paperCount < 350000 && paperCount >= 200000:
-            return "Supplier";
+            return DopeRoles.Supplier;
         case paperCount < 200000 && paperCount >= 100000:
-            return "Dealer";
+            return DopeRoles.Dealer;
         case paperCount < 100000 && paperCount >= 50000:
-            return "Hopper";
+            return DopeRoles.Hopper;
         case paperCount < 50000 && paperCount >= 12500:
-            return "Prospect"
+            return DopeRoles.Prospect;
         default:
             return null;
     }
 }
 
 export const startRedisSub = async (discClient: Client): Promise<void> => {
-    throw new Error("\n\n\n\n\n\n\n\n\n\nYou are running the wrong bot mane...");
+    //throw new Error("\n\n\n\n\n\n\n\n\n\nYou are running the wrong bot mane...");
 
     try {
         const client = createClient();
-        await client.connect();
-
-        const subscriber = client.duplicate();
-        await subscriber.connect();
 
         client.on('error', (err) => log.error(err));
 
-        await subscriber.subscribe("discord", async (message) => {
-            const discUser = JSON.parse(message) as IDiscordUser;
-            const discUserName = `${discUser.username}#${discUser.discriminator}`;
+        await client.subscribe("discord", async (message) => {
+            const verifData = JSON.parse(message) as IDopeBotData;
 
-            log.info(`${discUser.id} - ${discUser.username}#${discUser.discriminator} - ${discUser.paperCount} - ${discUser.dopeCount} - ${discUser.hustlerCount} - ${discUser.isOg}`);
+            log.info(`Verifying ${verifData.Id} - ${verifData.DopeCount} - ${verifData.HustlerCount} - ${verifData.PaperCount} - ${verifData.IsOg}`);
 
-            const guild = discClient.guilds.resolve(process.env.DBOT_GUILD_ID!);
-            const members = await guild!.members.fetch({ force: true });
-            const user = members.get(discUser.id);
+            const guild = discClient.guilds.cache.get(process.env.DBOT_GUILD_ID!);
+            const guildMember = guild?.members.cache.get(verifData.Id);
 
-            if (!user) {
-                log.error(`User: ${discUserName} not in guild`);
-                return;
+            const role = getPaperRole(verifData.PaperCount);
+            guildMember?.roles.add(String(role));
+
+            if (verifData.IsOg) {
+                guildMember?.roles.add(String(ogRole));
+            }
+
+            if (verifData.DopeCount > 0) {
+                guildMember?.roles.add(String(dopeHolderRole));
+            }
+
+            if (verifData.HustlerCount> 0) {
+                guildMember?.roles.add(String(hustlerHolderRole));
             }
         });
     } catch (error: unknown) {
